0875d16de67967c3e984038e4211bddfbe370fc3
caprice309 <caprice.arjhan.elderflame@gmail.com>
2025-10-09 20:10:07 -0300

    Teste 2

---
+++ Funcoes.h
@@ -0,0 +1,32 @@
+/*
+Autor...: José Luiz Delgado Tavares
+Data....: 06/10/2025
+Membros.: RA - Enzo Gati Barbaresco
+          RA - José Luiz Delgado Tavares
+Objetivo: Deixar todos os arquivos salvos aqui
+*/
+
+//Começo do arquivo .h
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+//Bibliotecas utilizadas
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <windows.h>
+#include <conio.h>
+
+//Declarar as voids aqui e e comentar o que cada uma faz
+
+//Definir a linha do cursor no eixo X e Y
+void gotoxy(int x, int y);
+
+//Mostrar a Tela
+void tela();
+
+//Fim do arquivo .h
+#endif
+
+//MSG: Nós estamos muito fudidos
---
+++ Tela.c
@@ -0,0 +1,58 @@
+/*Autor...: José Luiz Delgado Tavares
+  Data....: 06/10/2025
+  Membros.: 268322-2024 : Enzo Gati Barbaresco
+            292595-2025 : Jose Luiz Delgado Tavares
+  Objetivo: Mostrar a tela
+*/
+
+#include "Funcoes.h"
+
+void tela(){
+    int i;
+    system ("color 0a");
+    system ("cls");
+    gotoxy(2,2);
+    printf("ENZO GATI BARBARESCO");
+    gotoxy(2,3);
+    printf("JOSE LUIZ DELGADO TAVARES");
+    gotoxy(61,2);
+    printf("ESTRUTURA DE DADOS");
+
+    for(i = 1; i <=24; i++){
+        gotoxy(1,i);
+        printf("|");
+        gotoxy(80,i);
+        printf("|");
+    }
+
+    for(int i = 1; i <=80; i++){
+        gotoxy(i,1);
+        printf("-");
+        gotoxy(i,4);
+        printf("-");
+        gotoxy(i,22);
+        printf("-");
+        gotoxy(i,24);
+        printf("-");
+    }
+
+    gotoxy(1,1);
+    printf("+");
+    gotoxy(80,1);
+    printf("+");
+    gotoxy(1,4);
+    printf("+");
+    gotoxy(80,4);
+    printf("+");
+    gotoxy(1,22);
+    printf("+");
+    gotoxy(80,22);
+    printf("+");
+    gotoxy(1,24);
+    printf("+");
+    gotoxy(80,24);
+    printf("+");
+
+    gotoxy(2,23);
+    printf("MSG.:");
+}
