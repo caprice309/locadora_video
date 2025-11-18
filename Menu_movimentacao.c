209d255788b0e7e42331687fcb38b02e916891c7
caprice309 <caprice.arjhan.elderflame@gmail.com>
2025-11-18 13:39:50 -0300

    Correção de erros

--- Menu_movimentacao.c
+++ Menu_movimentacao.c
@@ -1,9 +1,9 @@
 /*
-  Autor...: José Luiz Delgado Tavares
+  Autor...: Enzo Gati Barbaresco
   Data....: 16/10/2025
   Membros.: 268322-2024 : Enzo Gati Barbaresco
             292595-2025 : Jose Luiz Delgado Tavares
-  Objetivo: Se a base de transição entre os diferentes menus de movimentação
+  Objetivo: Se a base de transiÃ§Ã£o entre os diferentes menus de movimentaÃ§Ã£o
 */
 
 #ifdef _MSC_VER
@@ -78,11 +78,11 @@ void menu_movimentacao() {
         int valido = 0;
 
         while (!valido || strlen(entrada) == 0) {
-            // lê a entrada como texto
+            // lÃª a entrada como texto
             fgets(entrada, sizeof(entrada), stdin);
             entrada[strcspn(entrada, "\n")] = '\0'; // remove o Enter
 
-            // verifica se a entrada contém apenas números
+            // verifica se a entrada contÃ©m apenas nÃºmeros
             valido = 1;
             for (int i = 0; i < strlen(entrada); i++) {
                 if (!isdigit((unsigned char)entrada[i])) {
@@ -106,7 +106,7 @@ void menu_movimentacao() {
             }
         }
 
-        // converte para inteiro apenas se for válido
+        // converte para inteiro apenas se for vÃ¡lido
         opcao = atoi(entrada);
 
         switch (opcao) {
@@ -131,4 +131,5 @@ void menu_movimentacao() {
         break;
         }
     } while (opcao != 5);
+
 }
